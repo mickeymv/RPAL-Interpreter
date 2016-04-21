ca1b22cb7d90d525165e3bb9d4dc3add48b7c167
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2016-04-21 12:13:44 -0400

    Correct behavior for handling children nodes of tuples. Default to flattening them without processing the children. Test case programs 'Innerprod', 'wsum1', 'Treepicture', 'tiny' passed.

--- rpal-interpreter.cpp
+++ rpal-interpreter.cpp
@@ -1446,7 +1446,7 @@ void recursivelyFlattenTree(Node *treeNode, list<MachineNode> *controlStructure,
                 recursivelyFlattenTree(tauElementNode->firstKid, controlStructure, controlStructureIndex, true, true);
                 cout << "\n size of controlStructure '" << controlStructureIndex << "' is= " <<
                 controlStructure->size();
-            } else if (tauElementNode->label == "tau") {
+            } else {
                 cout << "\n it's a " << tauElementNode->label;
                 recursivelyFlattenTree(tauElementNode, controlStructure, controlStructureIndex, true, false);
             }
